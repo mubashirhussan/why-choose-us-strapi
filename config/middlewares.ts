25a151c8b2cb7cd1148d553a5775b13bdea10580
mubashirhussan <mubashirhussan93@gmail.com>
2025-04-23 18:13:05 +0500

    build update

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,18 +2,18 @@ export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://win.kshopinn.com"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      headers: "*",
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: ["https://win.kshopinn.com/"],
-      methods: ["GET", "POST", "PUT", "DELETE"],
-    },
-  },
 ];
